c68edcbe23585bfaa14cf3dfaf8c0cb13d78ace9
Brennan <flood.brennan@gmail.com>
2019-12-13 16:02:13 -0500

    fix auth services and add register mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,10 +1,22 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
 const mongoose = require("mongoose");
-
+const AuthService = require("../services/auth")
 const mutation = new GraphQLObjectType({
   name: "Mutation",
-  fields: {}
+  fields: {
+    register: {
+      type: UserType,
+      args: {
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(_, args) {
+        return AuthService.register(args);
+      }
+    }
+  }
 });
 
 module.exports = mutation;
