5879d3b4b25b73688649ce4f7e19d90b0d2e002e
aparcanapavel <aparcanapavel@gmail.com>
2019-12-13 18:57:32 -0500

    user registration

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { HashRouter } from "react-router-dom";
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
@@ -18,7 +17,11 @@ const cache = new InMemoryCache({
 });
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000/graphql"
+  uri: "http://localhost:5000/graphql",
+  headers: {
+    // pass our token into the header of each request
+    authorization: localStorage.getItem("auth-token")
+  }
 });
 
 const errorLink = onError(({ graphQLErrors }) => {
