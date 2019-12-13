864725698996de07969e65b6056229068440d856
Brennan <flood.brennan@gmail.com>
2019-12-13 15:44:21 -0500

    add register services

---
+++ server/services/auth.js
@@ -0,0 +1,38 @@
+const register = async data => {
+  try {
+    const { message, isValid } = validateRegisterInput(data);
+
+    if (!isValid) {
+      throw new Error(message);
+    }
+    const { name, email, password } = data;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new Error("This user already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User(
+      {
+        name,
+        email,
+        password: hashedPassword
+      },
+      err => {
+        if (err) throw err;
+      }
+    );
+
+    user.save();
+    // we'll create a token for the user
+    const token = jwt.sign({ id: user._id }, keys.secretOrKey);
+
+    // then return our created token, set loggedIn to be true, null their password, and send the rest of the user
+    return { token, loggedIn: true, ...user._doc, password: null };
+  } catch (err) {
+    throw err;
+  }
+};
