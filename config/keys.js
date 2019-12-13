a27429baac3d30f228ecfc95332b0745be942b05
aparcanapavel <aparcanapavel@gmail.com>
2019-12-13 14:22:08 -0500

    update git ignore and setup of user auth

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGO_URI:
-    "mongodb+srv://studi-fi-dev:2CyGCDUA551V6lUk@cluster0-duni4.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "04fd6b91-5873-481c-8fae-d10c05e08a24"
-};
-
