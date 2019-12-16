b037116592ee0434a703ef025ec4571032f98a53
Brennan <flood.brennan@gmail.com>
2019-12-16 15:44:19 -0500

    add song index skeleton

---
+++ client/src/components/song/song_index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SongIndex extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1>SongIndex</h1>
+    </div>
+    )
+  }
+}
+
+export default SongIndex;
