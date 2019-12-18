8b5beee9609aa339d2e339220956d2c87fd0d9fd
aparcanapavel <aparcanapavel@gmail.com>
2019-12-18 11:46:33 -0500

    add mock home component and edit main nav.

---
+++ client/src/components/main_app/home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class HomeComponent extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <h3>this is the home component</h3>
+    )
+  }
+}
+
+export default HomeComponent;
