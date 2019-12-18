4c680e5f19560257455c0fd18f9731ffe4acd86a
aparcanapavel <aparcanapavel@gmail.com>
2019-12-18 12:13:30 -0500

    move the music player to be with in the main component in order to controll it

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -24,7 +24,6 @@ const App = () => {
           return(
             <div className='main-component'>
               <MainComponent />
-              <MusicPlayer />
             </div>
           )
         } else {
