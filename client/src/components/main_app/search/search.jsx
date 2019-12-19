e50a7dba46342b6c2483cdd118297ac843d592c9
aparcanapavel <aparcanapavel@gmail.com>
2019-12-19 12:50:38 -0500

    displays empty search message when no search has been invoked

--- client/src/components/main_app/search/search.jsx
+++ client/src/components/main_app/search/search.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Query } from 'react-apollo';
+import Queries from '../../../graphql/queries';
+const { FETCH_ALL } = Queries;
 
 class Search extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      search: ""
+      search: "",
+      results: null
     }
     this.updateSearch = this.updateSearch.bind(this);
   }
@@ -24,12 +28,29 @@ class Search extends React.Component {
             id="search-field"
             type="text"
             value={this.state.search}
-            onChange={this.updateSearch}
+            onChange={this.updateSearch()}
+            placeholder="Search for Artists, Songs, or Albums"
           />
           <label htmlFor="search-field" className="search-field-x">
-            x
+            X
           </label>
         </form>
+        <Query query={FETCH_ALL}>
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error</p>;
+
+            if(this.state.search === ""){
+              return (
+                <div className="empty-search">
+                  <h3>Start typing to begin searching our library!</h3>
+                </div>
+              )
+            } else {
+
+            }
+          }}
+        </Query>
       </section>
     );
   }
