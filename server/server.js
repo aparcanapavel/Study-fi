c013c1ff86096c919deeffba897922a3021ff9fa
Brennan <flood.brennan@gmail.com>
2020-01-06 16:19:47 -0500

    production commit

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ const schema = require("./schema/schema");
 const cors = require("cors");
 const expressGraphQL = require("express-graphql");
 const app = express();
-const path = require('path');
+// const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-};
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('frontend/build'));
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   })
+// };
 
 mongoose
   .connect(db, { useNewUrlParser: true })
