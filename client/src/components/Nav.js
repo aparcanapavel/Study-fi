1025776c4137f88f4958a7c8a5eb64a01204399f
aparcanapavel <aparcanapavel@gmail.com>
2020-01-09 19:39:47 -0500

    home blur added

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -23,6 +23,7 @@ const Nav = props => {
                 >
                   Logout
                 </button>
+                <div className="blurred"></div>
                 </div>
               );
             } else {
